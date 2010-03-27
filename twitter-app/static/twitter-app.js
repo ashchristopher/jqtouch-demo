34fd09666462521bcd5dac0ba7b1e425ccc151bf
Ash Christopher <ashc@sandbox.localdomain>
2010-03-27 18:40:30 -0400

    Added a refresh button

--- twitter-app/static/twitter-app.js
+++ twitter-app/static/twitter-app.js
@@ -4,26 +4,34 @@ $.jQTouch({
 });
 
 $(document).ready(function() {
-    var the_data;
+    function update_tweets(){
+        var $tweets = $('#tweets');
+
+        // load the tweets
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: '/tweets',
+            success: function(data, textStatus, XMLHttpRequest) {
+                $tweets.empty();
+                data.forEach(function(tweet){
+                    $tweets.append("<li><img src='" + tweet.user.profile_image_url + "'/>" + tweet.text + "</li>")
+                });
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown){
+                alert('error');
+            }
+        });
+    }
     
     $("#the-tweets").bind("pageAnimationEnd", function (e, info) {
         if (info.direction == 'in') {
-            var $tweets = $('#tweets');
-            // load the tweets
-            $.ajax({
-                type: 'GET',
-                dataType: 'json',
-                url: '/tweets',
-                success: function(data, textStatus, XMLHttpRequest) {
-                    $tweets.empty();
-                    data.forEach(function(tweet){
-                        $tweets.append("<li><img src='" + tweet.user.profile_image_url + "'/>" + tweet.text + "</li>")
-                    });
-                },
-                error: function(XMLHttpRequest, textStatus, errorThrown){
-                    alert('error');
-                }
-            });
+            update_tweets();
         }
+    });
+    
+    $('#refresh').tap(function(e) {
+        e.preventDefault();
+        update_tweets();
     });
 });
